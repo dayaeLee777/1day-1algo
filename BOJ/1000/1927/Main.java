c89451cc795f9e1015c8feb5776641b480224618
Daye <ldy1853@naver.com>
2022-01-14 23:55:38 +0900

    Update BOJ 1927

---
+++ BOJ/1000/1927/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		N = sc.nextInt();
+		int input = 0;
+		for (int n = 0; n < N; n++) {
+			input = sc.nextInt();
+			if (input == 0) {
+				if (pq.isEmpty())
+					sb.append(0).append("\n");
+				else
+					sb.append(pq.poll()).append("\n");
+			} else
+				pq.offer(input);
+
+		}
+
+		System.out.println(sb.toString());
+	}
+}
